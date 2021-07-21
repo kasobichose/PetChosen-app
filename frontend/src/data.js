56ac86ced904f1b37959b9132d42c1de39dcfdc2
kasipeters <kasobichosen@gmail.com>
2021-07-21 14:09:49 -0700

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirt',
+            image:'/images/product-1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Fit Shirt',
+            category:'Shirts',
+            image:'/images/product-2.jpg',
+            price:100,
+            brand:'Adidas',
+            rating:4.6,
+            numReviews:12,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste Free Shirt',
+            category:'Shirts',
+            image:'/images/product-3.jpg',
+            price:220,
+            brand:'Lacoste',
+            rating:4.8,
+            numReviews:17,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'Pants',
+            image:'/images/product-4.jpg',
+            price:200,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:14,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pant',
+            category:'Pants',
+            image:'/images/product-5.jpg',
+            price:90,
+            brand:'Puma',
+            rating:4.0,
+            numReviews:20,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Adidas Fit Pant',
+            category:'Pants',
+            image:'/images/product-6.jpg',
+            price:110,
+            brand:'Adidas',
+            rating:4.9,
+            numReviews:16,
+            description:'high quality product'
+        }
+    ]
+}
+export default data;
